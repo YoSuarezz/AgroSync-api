4712a739e8ee3dd3982e24f0b0900ffcc6138ffb
David-alzate <alzatedavid0126@gmail.com>
2025-08-08 22:26:54 -0500

    ctualización de la ruta de la API de usuarios a una versión simplificada

--- src/main/java/com/agrosync/infrastructure/rest/controller/usuarios/UsuarioController.java
+++ src/main/java/com/agrosync/infrastructure/rest/controller/usuarios/UsuarioController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/v1/usuarios")
+@RequestMapping("/api/usuarios")
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
