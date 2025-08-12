12c4d8e496d9ffc9fba19e519ce2f650f591a427
David-alzate <alzatedavid0126@gmail.com>
2025-08-12 13:09:51 -0500

    Se actualizo la manera en como se inicializan los datos de la aplicacion

--- src/main/java/com/agrosync/AgroSyncApplication.java
+++ src/main/java/com/agrosync/AgroSyncApplication.java
@@ -1,16 +1,34 @@
 package com.agrosync;
 
+import com.agrosync.application.secondaryports.entity.usuarios.TIpoUsuarioEntity;
+import com.agrosync.application.secondaryports.repository.TipoUsuarioRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @SpringBootApplication
 public class AgroSyncApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AgroSyncApplication.class, args);
+    }
+
+
+    @Bean
+    CommandLineRunner init(TipoUsuarioRepository tipoUsuarioRepository) {
+        return args -> {
+            if (tipoUsuarioRepository.count() == 0) {
+                tipoUsuarioRepository.saveAll(List.of(
+                        TIpoUsuarioEntity.create().setId(1L).setNombre("Cliente"),
+                        TIpoUsuarioEntity.create().setId(2L).setNombre("Proveedor")
+                ));
+            }
+        };
     }
 
     @Bean
