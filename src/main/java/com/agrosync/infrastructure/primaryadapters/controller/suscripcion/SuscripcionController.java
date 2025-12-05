244588521ddd4a82bcff80fb94f63a0f9aed0c38
David-Alzate <alzatedavid0126@gmail.com>
2025-12-05 00:59:37 -0500

    cambia nombre de la ruta a español

--- src/main/java/com/agrosync/infrastructure/primaryadapters/controller/suscripcion/SuscripcionController.java
+++ src/main/java/com/agrosync/infrastructure/primaryadapters/controller/suscripcion/SuscripcionController.java
@@ -19,7 +19,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/subscription")
+@RequestMapping("/suscripcion")
 public class SuscripcionController {
 
     private final ObtenerSuscripcionesInteractor obtenerSuscripcionesInteractor;
