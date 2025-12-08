1e0d0f0bc61dce510542a5a2bced9ff401596525
Sebastian <sebassuarez103@gmail.com>
2025-12-07 19:51:09 -0500

    se implementa la gestión de cuentas por cobrar, incluyendo DTOs, mappers, reglas de validación y controladores para la consulta y obtención de cuentas

--- src/main/java/com/agrosync/application/usecase/cuentascobrar/rulesvalidator/ObtenerCuentaCobrarPorIdRulesValidator.java
+++ src/main/java/com/agrosync/application/usecase/cuentascobrar/rulesvalidator/ObtenerCuentaCobrarPorIdRulesValidator.java
@@ -1,7 +1,6 @@
 package com.agrosync.application.usecase.cuentascobrar.rulesvalidator;
 
 import com.agrosync.application.primaryports.dto.cuentascobrar.request.CuentaCobrarIdSuscripcionDTO;
-import com.agrosync.application.primaryports.dto.cuentaspagar.request.CuentaPagarIdSuscripcionDTO;
 import com.agrosync.application.usecase.RulesValidator;
 
 public interface ObtenerCuentaCobrarPorIdRulesValidator extends RulesValidator<CuentaCobrarIdSuscripcionDTO> {
