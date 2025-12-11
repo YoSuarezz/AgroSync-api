2bebdd1e685fb5e69ddc940e6535f620fdef3768
David-Alzate <alzatedavid0126@gmail.com>
2025-12-11 07:47:22 -0500

    se arregla el Mapper de usuarios para la correcta consulta

--- src/main/java/com/agrosync/application/secondaryports/mapper/usuarios/UsuarioEntityMapper.java
+++ src/main/java/com/agrosync/application/secondaryports/mapper/usuarios/UsuarioEntityMapper.java
@@ -3,19 +3,21 @@ package com.agrosync.application.secondaryports.mapper.usuarios;
 import com.agrosync.application.secondaryports.entity.usuarios.UsuarioEntity;
 import com.agrosync.domain.usuarios.UsuarioDomain;
 import com.agrosync.application.secondaryports.entity.suscripcion.SuscripcionEntity;
+import com.agrosync.application.secondaryports.mapper.carteras.CarteraEntityMapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 import java.util.List;
 import java.util.UUID;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {CarteraEntityMapper.class})
 public interface UsuarioEntityMapper {
-    
+
     @Mapping(target = "suscripcion", source = "suscripcionId")
     UsuarioEntity toEntity(UsuarioDomain domain);
 
     @Mapping(target = "suscripcionId", source = "suscripcion.id")
+    @Mapping(target = "cartera.usuario", ignore = true)
     UsuarioDomain toDomain(UsuarioEntity entity);
 
     List<UsuarioEntity> toEntityCollection(List<UsuarioDomain> domainCollection);
