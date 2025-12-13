5844a0dbfe40d14894ea5b5c0e3e188b656be48b
Juan Diego Duque <juandiegoduquejimenez@gmail.com>
2025-12-13 18:29:05 -0500

    Se creó y probó la funcionalidad de obtener carteras

---
+++ src/main/java/com/agrosync/application/primaryports/mapper/carteras/CarteraDTOMapper.java
@@ -0,0 +1,32 @@
+package com.agrosync.application.primaryports.mapper.carteras;
+
+import com.agrosync.application.primaryports.dto.carteras.response.ObtenerCarteraDTO;
+import com.agrosync.application.primaryports.mapper.usuarios.UsuarioDTOMapper;
+import com.agrosync.domain.carteras.CarteraDomain;
+import com.agrosync.domain.usuarios.UsuarioDomain;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+
+import java.util.List;
+import java.util.UUID;
+
+@Mapper(uses = {UsuarioDTOMapper.class})
+public interface CarteraDTOMapper {
+
+    CarteraDTOMapper INSTANCE = Mappers.getMapper(CarteraDTOMapper.class);
+
+    ObtenerCarteraDTO toObtenerDTO(CarteraDomain domain);
+
+    List<ObtenerCarteraDTO> toObtenerDTOCollection(List<CarteraDomain> domainCollection);
+
+    default Page<ObtenerCarteraDTO> toObtenerDTOCollection(Page<CarteraDomain> domainPage) {
+        List<ObtenerCarteraDTO> dtoList = toObtenerDTOCollection(domainPage.getContent());
+        return new PageImpl<>(dtoList, domainPage.getPageable(), domainPage.getTotalElements());
+    }
+
+    default UsuarioDomain map(UUID usuarioId) {
+        return UsuarioDomain.create(usuarioId);
+    }
+}
